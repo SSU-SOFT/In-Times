d2600edcd7c8571d5bf097c8a78b1f96e85e071e
Rude_zoo <zoo5871@naver.com>
2021-09-04 18:20:03 +0900

    pass cluster Number [2021.09.04]

--- client/src/components/MainPage/SideMenu.js
+++ client/src/components/MainPage/SideMenu.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../../style/SideMenu.css";
 import { useRecoilState} from 'recoil';
+import { cIdState } from "../../state/state";
 import { yearState } from "../../state/state";
 
 const SideMenu=()=>{
 
     const [year,setYear]=useRecoilState(yearState);
+    const [cId, setCid ] = useRecoilState(cIdState);
 
     return(
         <>
-        SideMenu {year}
+        SideMenu {year}, {cId}
         </>
     );
 }
--- client/src/state/state.js
+++ client/src/state/state.js
@@ -5,3 +5,8 @@ export const yearState = atom({
     key: 'yearState',
     default: 0,
 });
+
+export const cIdState = atom({
+    key: 'cIdState',
+    default: 0,
+});
